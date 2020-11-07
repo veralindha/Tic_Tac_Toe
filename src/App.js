faa86150b9b702dc7630183cdc5dbf3c7cf395a6
veralindha <fangkyistiawan@gmail.com>
2020-11-07 12:01:11 +0700

    tes

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+import TicTacToe from "./TicTacToe";
+import "papercss/dist/paper.min.css";
+import anime from "animejs";
+
+function App() {
+  return (
+    <>
+      <Main>
+        <TicTacToe />
+      </Main>
+      <Footer>
+        <FooterInner>
+          TicTacToe Fangky Ristiawan (1119101748){" "}
+          <a href="https://github.com/veralindha">Github</a><br></br>
+        </FooterInner>
+      </Footer>
+    </>
+  );
+}
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1 0 auto;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  flex: 0 0 auto;
+`;
+
+const FooterInner = styled.div`
+  padding: 16px 0;
+`;
+
+// const anime = require('animejs');
+
+// anime({
+//   targets: 'div',
+//   translateX: 250,
+//   rotate: '1turn',
+//   backgroundColor: '#FFF',
+//   duration: 800
+// });
+
+export default App;
